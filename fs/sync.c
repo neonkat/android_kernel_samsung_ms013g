c1e9c8df757391dfdcfbca921119363a0a392585
neonkat <neonkatxda@gmail.com>
2016-04-30 17:01:54 +0530

    fixes

--- fs/sync.c
+++ fs/sync.c
@@ -23,7 +23,6 @@ extern bool dyn_fsync_active;
 #endif
 
 bool fsync_enabled = true;
-module_param(fsync_enabled, bool, 0755);
 
 #define VALID_FLAGS (SYNC_FILE_RANGE_WAIT_BEFORE|SYNC_FILE_RANGE_WRITE| \
 			SYNC_FILE_RANGE_WAIT_AFTER)
